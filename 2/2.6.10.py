037f1d78e47e3d4e718f13a607afc43c0606460c
GentaP <gentapapakroni@yahoo.com>
2025-05-03 22:31:13 +0200

    ndryshim folders

--- 2/2.6.10.py
+++
@@ -1,6 +0,0 @@
-x = float(input("Enter value for x: "))
-
-y = 1/(x+1/(x+1/(x+1/x)))# Write your code here.
-
-print("y =", y)
-
--- 2/2.6.11.py
+++
@@ -1,6 +0,0 @@
-hour = int(input("Starting time (hours): "))
-mins = int(input("Starting time (minutes): "))
-dura = int(input("Event duration (minutes): "))
-
-print("End time:", (hour + (mins+dura)//60)%24,":", (mins+dura)%60) # Write your code here.
-
