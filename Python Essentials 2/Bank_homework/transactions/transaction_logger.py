4fdbc63807b0570fe47bb5e34bcb9c216caa6dda
GentaP <gentapapakroni@yahoo.com>
2025-06-04 17:39:22 +0200

    draft 3

--- Python Essentials 2/Bank_homework/transactions/transaction_logger.py
+++ Python Essentials 2/Bank_homework/transactions/transaction_logger.py
@@ -1,8 +1,19 @@
+from os import strerror
+import os
+
+
 class TransactionLogger:
     def __init__(self):
-        pass
+        # Ensure the data directory exists
+        if not os.path.exists("data"):
+            os.makedirs("data")
 
     def log_transaction(self,customer_name, action, amount:int):
-        with open("data/transaction_log.txt", "a") as file:
-            file.write(f"{customer_name} | {action} | {amount}\n")
+        try:
+            with open("data/transaction_log.txt", "a") as file:
+                file.write(f"{customer_name} | {action} | {amount}\n")
+
+        except Exception as exc:
+            print("The file could not be opened:", strerror(exc.errno))
+
 
